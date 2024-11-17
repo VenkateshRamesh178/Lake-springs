5745872f573b6f672847dd6953c9cc4cd39a04a6
venkateshramesh178 <venkateshramesh178@gmail.com>
2024-11-17 20:45:52 +0530

    final commit, mostly

--- src/Components/Amenities.js
+++ src/Components/Amenities.js
@@ -4,14 +4,13 @@ import pool from '../assets/pool.jpg';
 import gym from '../assets/gym.jpg';
 import playpen from '../assets/playpen.jpg';
 import hall from '../assets/hall.jpg';
-import more from '../assets/more.jpg';
 
 const amenitiesList = [
   { name: 'Party Hall', img: hall },
   { name: 'Swimming Pool', img: pool },
   { name: 'Gymnasium', img: gym },
   { name: 'Kids Play Area', img: playpen },
-  { name: 'And many more', img: more}
+  { name: 'and many more', customContent: '30+' }, // Special card
 ];
 
 const Amenities = () => {
@@ -21,7 +20,11 @@ const Amenities = () => {
       <div className="amenities-grid">
         {amenitiesList.map((amenity, index) => (
           <div key={index} className="amenity-card">
-            <img src={amenity.img} alt={amenity.name} />
+            {amenity.img ? (
+              <img src={amenity.img} alt={amenity.name} />
+            ) : (
+              <div className="custom-content">{amenity.customContent}</div>
+            )}
             <h3>{amenity.name}</h3>
           </div>
         ))}
